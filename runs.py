eabca027a118dbfcad002198643b85b3370e6d84
StarBlitz840 <starblitz.840@gmail.com>
2025-12-14 16:48:59 +0200

    b

--- runs.py
+++ runs.py
@@ -17,35 +17,59 @@ chasis.use_gyro(True)
 
 chasis.settings(500)
 
-runs = hub_menu("1", "2", "3", "4", "5")
+selected = hub_menu("1", "2", "3", "4", "5")
+
+0
 
 def run1():
+    right_arm.run_time(10000 ,2000 )
     chasis.straight(600)
-    right_arm.run_time (10000 ,2000 )
-    right_arm.run_time (-10000 ,2000 )
-# run1()
+    right_arm.run_time(-10000 ,2000 )
+
+def run2():
+    chasis.straight(600)
+    chasis.turn(90)
+    chasis.straight(100)
+    chasis.turn(-90)
+    chasis.straight(50)
+    
+    chasis.straight(-150)
+    chasis.straight(100)
+    chasis.straight(-100)
+    chasis.straight(100)
+    chasis.straight(-100)
+    chasis.straight(100)
+    chasis.straight(-100)
+    chasis.straight(100)
+    chasis.straight(-100)
+    chasis.straight(100)
+    # chasis.straight(200)
+    # chasis.straight(-200)
+
 def run3():
 
     chasis.straight(880)
     right_arm.run_angle(500, -180)
     chasis.straight(-860)
-# run3()
+
 
 def run5():
-    # right_arm.run_time(-300, 5000)
     chasis.straight(920)
     chasis.turn(90)
     chasis.straight(425)
     chasis.turn(-90)
     left_arm.run_time(-2000,2500)
     left_arm.run_time(2000,2500)
+    right_arm.run_time(-300, 5000)
 
-if runs == "1":
+if selected == "1":
     run1()
 
-if runs == "3":
+if selected == "2":
+    run2()
+
+if selected == "3":
     run3()
 
-if runs == "5":
+if selected == "5":
     run5()
-run1
