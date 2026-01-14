a7a4ce13ae8adc2e49c1f62f2c2df4ff061e8e81
StarBoi840! <starblitz.840@gmail.com>
2026-01-14 16:48:41 +0200

    bhjgu

--- runs.py
+++ runs.py
@@ -12,17 +12,20 @@ right_wheel = Motor(Port.B)
 left_arm = Motor(Port.E)
 right_arm = Motor(Port.A)
 map_sensor = ColorSensor(Port.D)
+arm_sensor = ColorSensor(Port.B)
+
+map_sensor.detectable_colors([Color.BLUE, Color.GRAY, Color.BROWN, Color.ORANGE, Color.RED])
 
 chasis = DriveBase(left_wheel, right_wheel, 80, 80)
 chasis.use_gyro(True)
 
 chasis.settings(300)
 
-selected = hub_menu("1", "2", "3", "4", "5")
+selected = hub_menu("1", "2", "3", "4", "5", "D")
 
 def until_black(): 
     chasis.drive(100, 0)
-    while map_sensor.color() != Color.NONE:
+    while map_sensor.reflection != 12:
         if map_sensor.color()== Color.NONE:
             break
         print(map_sensor.color())
@@ -34,16 +37,16 @@ def run1():
     for i in range(4):
         right_arm.run_time(1000, 770)
         right_arm.run_time(-1000, 735)
-    right_arm.run_angle(400, 80)
+    right_arm.run_angle(400, 100)
     until_black()
-    chasis.turn(-72)
-    chasis.straight(140)
-    chasis.turn(45)
-    # chasis.straight(95)
-    # chasis.turn(48)
+    chasis.turn(-65)
+    chasis.straight(145)
+    chasis.turn(-45)
+    chasis.straight(95)
+    chasis.turn(-100)
 
 
-def run2():
+def run2(): 
     chasis.straight(-90)
     chasis.straight(90)
     chasis.straight(-90)
@@ -68,7 +71,7 @@ def run3():
     chasis.straight(-860)
 
 
-def run5():
+def run4():
     chasis.straight(820)
     chasis.turn(90)
     chasis.straight(425)
@@ -76,6 +79,14 @@ def run5():
     left_arm.run_time(-2000,2500)
     left_arm.run_time(2000,2500)
     right_arm.run_time(-600, 5000)
+
+def run5():
+    chasis.straight(790)
+
+def detect_run():
+    if arm_sensor.color(True) == Color.BLUE:
+        run3()
+    #elif
 
 if selected == "1":
     run1()
@@ -86,5 +97,11 @@ if selected == "2":
 if selected == "3":
     run3()
 
+if selected == "4":
+    run4()
+
 if selected == "5":
     run5()
+
+if selected == "D":
+    detect_run()
